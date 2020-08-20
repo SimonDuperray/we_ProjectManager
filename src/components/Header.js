a9535d145698c413f15c30c409788c26db266334
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-08-20 21:11:04 +0200

    ToggleInfoHeader V2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-
-// const Header = ({ pseudo, cpt }) => {
-//     return (
-//         <header>
-//             <div className="rows">
-//                 <h1 className="row">{pseudo}'s Project Manager</h1>
-//                 <button id="revealHeader" className="row">+</button>
-//             </div>
-//             <h3 id="cptProj">{cpt} projets en cours!</h3>
-//             <a id="fireLink" target="_blank" href="https://console.firebase.google.com/project/projectmanager-67f9c/database/projectmanager-67f9c/data">RealTime DataBase - Firebase</a>
-//         </header>
-//     )
-// }
-
-// export default Header;
-
-// CLASS COMPONENT TRANSFORM
-
 import React from 'react'
 
 class Header extends React.Component {
