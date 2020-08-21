e8d5af3e0bffe7d5b10e0fab72ee2ce651056ece
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-08-21 13:53:53 +0200

    Card -> class component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Card = ({ details }) => {
-    const notes = details.notes
+class Card extends Component {
+    state = {
+        checkboxState: false,
+    }
+    render() {
+        const { details } = this.props
+        const notes = details.notes
         .split('\n')
-        .map(item => <li key={item}>{item}</li>)
-    // const notes = details.notes
-    //     .split('\n')
-    //     .map(item => {
-    //         {
-    //             console.log(details.notes[0])
-    //         }
-    //     })
-    return (
-        <div className="card">
-            <div className="recette">
-                <h2>{details.nom}</h2>
-                <div className="description">
-                    <h4>{ details.description }</h4>
-                </div>
-                <ul>
-                    { notes }
-                </ul>
+        .map(item => 
+            <div className="rows">
+                <li className="row" key={item}>{item}</li>
+                <input className="row" type="checkbox"/>
             </div>
-        </div>
-    )
+        )
+        return (
+            <div className="card">
+                <div className="recette">
+                    <h2>{details.nom}</h2>
+                    <div className="description">
+                        <h4>{ details.description }</h4>
+                    </div>
+                    <ul>
+                        { notes }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Card
