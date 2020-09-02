225a2b22adab5d2cb937704447c4e7d1fdbbf112
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-09-02 08:41:43 +0200

    Updates

---
+++ precache-manifest.655ef5103677fd215d0d3dd866db20df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2c390d4e4261e12aca7",
+    "url": "/ProjectManager/static/css/main.3232676b.chunk.css"
+  },
+  {
+    "revision": "a2c390d4e4261e12aca7",
+    "url": "/ProjectManager/static/js/main.a2c390d4.chunk.js"
+  },
+  {
+    "revision": "d62e6be4389e4cb24e91",
+    "url": "/ProjectManager/static/js/1.d62e6be4.chunk.js"
+  },
+  {
+    "revision": "c82fc195bad3d0f5146e",
+    "url": "/ProjectManager/static/js/runtime~main.c82fc195.js"
+  },
+  {
+    "revision": "b20574b98b042b057a3091ac39f3668b",
+    "url": "/ProjectManager/index.html"
+  }
+];
