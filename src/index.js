832ca1ea6af1c4b17a92cef507cedadb935b1427
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-09-03 13:03:05 +0200

    TestProcess

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ import {
 const Root = () => (
   <Router>
     <Switch>
-      <Route exact path='http://Kartodix.github.io/ProjectManager' component={Connexion} />
+      <Route exact path={process.env.PUBLIC_IRL + '/kartodix.github.io/ProjectManager/'} />
+      {/* <Route exact path='http://Kartodix.github.io/ProjectManager' component={Connexion} /> */}
       {/* <Route exact path='/' component={Connexion} /> */}
       <Route path='/pseudo/:pseudo' component={App} />
       <Route component={NotFound} />
